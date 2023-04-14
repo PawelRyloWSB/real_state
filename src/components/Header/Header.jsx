25f9d05fb93b760e1a92cc5231dadd8666afbf5f
Nico Rozo <nicolas.arenas.rozo@gmail.com>
2023-04-14 13:30:26 +0200

    stuff

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <section className="header-wrapper">
+            <div className="header-container">
+                <img src="" alt="" />
+            </div>
+        </section>
+    )
+}
