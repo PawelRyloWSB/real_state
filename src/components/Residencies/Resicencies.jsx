6c48b172f8b5615652a41bb6221327263840edab
Nico Rozo <nicolas.arenas.rozo@gmail.com>
2023-04-19 12:15:04 +0200

    dependencies add

---
+++ src/components/Residencies/Resicencies.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react'
+import 'swiper/css'
+import './Residencies.css'
+
+export default function Residencies() {
+    return (
+        <div className="temporal">
+
+        </div>
+    )
+}
