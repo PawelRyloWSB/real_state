368d1b2b68877e12c98202b70898870fa8a82a33
Nico Rozo <nicolas.arenas.rozo@gmail.com>
2023-04-21 12:51:41 +0200

    vite onfig

--- src/components/Residencies/Resicencies.jsx
+++ src/components/Residencies/Resicencies.jsx
@@ -9,29 +9,38 @@ export default function Residencies() {
     return (
         <section className="res-wrapper">
             <div className="paddings innerWidth res-container">
-                <div className="res-head flexColStart">
+                <div className=" flexColStart res-head">
                     <span className="small-head-span">Best Choices</span>
                     <span className="big-head-span">Popular Residencies</span>
                 </div>
                 <Swiper {...sliderSettings}>
-                    {
-                        data.map((card, i) => (
-                            <SwiperSlide key={i} >
-                                <div className="res-card flexColStart">
-                                    <img src={card.image} alt="house" />
+                    <SliderButtons />
+                    {data.map((card, i) => (
+                        <SwiperSlide key={i} >
+                            <div className="res-card flexColStart">
+                                <img src={card.image} alt="house" />
 
-                                    <span className="secondaryText res-price">
-                                        <span style={{ color: "orangered" }}>$</span><span>{card.price}</span>
-                                    </span>
+                                <span className="secondaryText res-price">
+                                    <span style={{ color: "orangered" }}>$</span><span>{card.price}</span>
+                                </span>
 
-                                    <span className="big-head-span card-name">{card.name}</span>
-                                    <span className="card-detail">{card.detail}</span>
-                                </div>
-                            </SwiperSlide>
-                        ))
+                                <span className="big-head-span card-name">{card.name}</span>
+                                <span className="card-detail">{card.detail}</span>
+                            </div>
+                        </SwiperSlide>
+                    ))
                     }
                 </Swiper>
             </div>
         </section>
+    )
+}
+const SliderButtons = () => {
+    const swiper = useSwiper()
+    return (
+        <div className="flexCenter res-button">
+            <button onClick={() => swiper.slidePrev()}>&lt;</button>
+            <button onClick={() => swiper.slideNext()}>&gt;</button>
+        </div>
     )
 }
